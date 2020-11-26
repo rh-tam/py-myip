718f9eb55203c376b947adf50824ce3bd3bbdf3e
nelson <nelson.lin@redhat.com>
2020-11-26 09:14:55 +0800

    add Id for distinguishing

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, jsonify
+import random
 
 application = Flask(__name__)
-
+identity = random.randint(10000, 100000)
 @application.route("/", methods=["GET"])
 def get_my_ip():
     return render_template(
         'index.html', ip = request.environ['REMOTE_ADDR'],
-        serverIP = request.host_url
+        serverIP = request.host_url,
+        ID = identity
         )
 
 @application.route("/api", methods=["GET"])
